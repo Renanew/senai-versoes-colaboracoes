45d8a46ec92ce99e56ad784ac7a19423573d622e
Renanew <renan_new@hotmail.com>
2021-09-09 20:36:18 -0300

    Atividade 1 da aula presencial

---
+++ atividade1aula.js
@@ -0,0 +1,26 @@
+const Peça = ['A','Alicate','Martelo'];
+var Peso = 4;
+var QuantidadeDePeça = 5;
+
+for(let i=0; i<Peça.length; i++){
+    if(Peça[i].length<3){
+       // console.log(Peça[i]);
+        console.log("Peça "+ Peça[i] + " não cadastrada, pois caracteres é inferior a 3. ");
+        console.log("");
+    }
+    else if ((Peça[i].length>=3)){
+    console.log("Peça cadastrada: " +Peça[i]);
+    }
+    }
+console.log("");
+if (Peso>100){
+    console.log("Pode realizar o cadastro da peça devido a peso ser "+ Peso +" gramas.");
+    console.log("");
+}
+    else console.log("Cadastro não realizado devido ao Peso ser "+ Peso +" gramas.");
+    console.log("");
+
+if (QuantidadeDePeça>10){
+    console.log("Capacidade insuficiente para realizar o cadastro.");
+}
+else console.log("Capacidade suficiente para realizar o cadastro.")
