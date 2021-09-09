11d6252de1ef7819279b1344431aec56cfa0498f
Renanew <renan_new@hotmail.com>
2021-09-09 20:58:31 -0300

    Aula 1 atividade Presencial

--- atividade1aula.js
+++ atividade1aula.js
@@ -1,26 +1,30 @@
-const Peça = ['A','Alicate','Martelo    '];
+const Peça = ['A','Alicate','Ala'];
 var Peso = 4;
-var QuantidadeDePeça = 12;
+var QuantidadeDePeça = 10;
 
 for(let i=0; i<Peça.length; i++){
-    if(Peça[i].length<3){
+    if(Peça[i].length<=3){
        // console.log(Peça[i]);
         console.log("Peça "+ Peça[i] + " não cadastrada, pois caracteres é inferior a 3. ");
         console.log("");
     }
-    else if ((Peça[i].length>=3)){
+    else if ((Peça[i].length>3)){
     console.log("Peça cadastrada: " +Peça[i]);
+    console.log("");
     }
     }
-console.log("");
-if (Peso>=100){
+
+if (Peso>100){
     console.log("Pode realizar o cadastro da peça devido a peso ser "+ Peso +" gramas.");
     console.log("");
 }
-    else console.log("Cadastro não realizado devido ao peso ser "+ Peso +" gramas.");
+    else if(Peso<=100){
+    console.log("Cadastro não realizado devido ao peso ser "+ Peso +" gramas.");
     console.log("");
-
-if (QuantidadeDePeça>=10){
+    } 
+if (QuantidadeDePeça>10){
     console.log("Capacidade "+ QuantidadeDePeça +" insuficiente para realizar o cadastro.");
 }
-else console.log("Capacidade "+ QuantidadeDePeça + "suficiente para realizar o cadastro.")
+else if (QuantidadeDePeça<=10){
+ console.log("Capacidade "+ QuantidadeDePeça + " suficiente para realizar o cadastro.");
+}
