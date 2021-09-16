872ffa1465c62d977dedf44731fa09deabe5fcdf
Renanew <renan_new@hotmail.com>
2021-09-16 20:25:55 -0300

    Atividade Aula 2 presencial

---
+++ aula2.js
@@ -0,0 +1,16 @@
+var numeroAluno = 10;
+
+for(let numero=0; numero<=numeroAluno;numero++){
+if (numero % 2 == 0) {
+
+    if (numero!=0) {
+        console.log("Numero é "+ numero + " par");
+        
+    }else{
+        console.log("Numero é "+ numero + " zero");
+    }
+
+}else{
+    console.log("Numero é "+ numero + " impar");
+}
+}
